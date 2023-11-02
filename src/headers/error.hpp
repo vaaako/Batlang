c28cdac96146bdf9285081f59ef5e6a105b49d2b
vaaako <matharth520@gmail.com>
2023-11-02 00:30:14 -0300

    error message log is now at batring

--- src/headers/error.hpp
+++ src/headers/error.hpp
@@ -12,46 +12,32 @@ enum ErrorType {
 };
 
 
-// TODO mark hash map as const later
 
 class Error {
 	public:
-		// Error(Position pos, Position pos_end, std::string name, std::string details);
-		Error(const ErrorType error_type, const Position pos, const std::string details);
-		inline std::string as_string() const {
-			return colorize(name + ": ", "bold_red")
-					+ colorize(details, "green")
-					+ colorize("\nFile: ", "gray")
-					+ colorize(pos.get_filename(), "yellow")
-					+ colorize(" at index position ", "white")
-					+ colorize(std::to_string(pos.get_index()), "blue")
-					+ ", line "
-					+ colorize(std::to_string(pos.get_line() + 1), "blue");
+		// Error(Position pos, Position pos_end, std::string name, std::string detail);
+		Error(const ErrorType error_type, const Position pos, const std::string detail);
+
+		inline std::string get_name() const {
+			return name;
+		}
+
+		inline std::string get_detail() const {
+			return detail;
+		}
+
+		inline Position get_pos() const {
+			return pos;
 		}
 
 	private:
 		Position pos;
 		std::string name;
-		std::string details;
-
-		std::unordered_map<std::string, std::string> color_hash = {
-			{ "reset",    "\033[0m"  },
-			{ "red",      "\033[31m" },
-			{ "bold_red", "\033[1;31m" },
-			{ "green",    "\033[32m" },
-			{ "yellow",   "\033[33m" },
-			{ "blue",     "\033[34m" },
-			{ "gray",     "\033[90m" },
-			{ "white",     "\033[37m" }
-		};
+		std::string detail;
 
 		std::unordered_map<ErrorType, std::string> error_hash = {
 			{ IllegalCharError,   "Illegal Character"  },
 			{ InvalidSyntaxError, "Invalid Syntax" },
 			{ RuntimeError,       "Runtime Error" },
 		};
-
-		inline std::string colorize(std::string text, std::string color) const {
-			return color_hash.at(color) + text + color_hash.at("reset");
-		}
 };
